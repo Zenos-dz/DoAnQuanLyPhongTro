1c205cee61e3168691dde5d7a009b2c8a4eaf493
Kourain <maiquyen16503@gmail.com>
2023-03-29 10:37:23 +0700

    ....

--- WindowsFormsApp3/phongtro.Designer.cs
+++ WindowsFormsApp3/phongtro.Designer.cs
@@ -38,11 +38,11 @@
             this.txtgiatien = new System.Windows.Forms.TextBox();
             this.dgvUsers = new System.Windows.Forms.DataGridView();
             this.btnAddnew = new System.Windows.Forms.Button();
-            this.btnExit = new System.Windows.Forms.Button();
             this.btncancel = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,10 +50,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(51, 52);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(38, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 18);
+            this.label1.Size = new System.Drawing.Size(69, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mã phòng: ";
             // 
@@ -61,10 +60,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label2.Location = new System.Drawing.Point(51, 207);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(38, 168);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 18);
+            this.label2.Size = new System.Drawing.Size(69, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "Tên phòng:";
             // 
@@ -72,10 +70,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label3.Location = new System.Drawing.Point(60, 128);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(45, 104);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 18);
+            this.label3.Size = new System.Drawing.Size(58, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "Diện tích:";
             // 
@@ -83,90 +80,71 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label4.Location = new System.Drawing.Point(67, 282);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(50, 229);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 18);
+            this.label4.Size = new System.Drawing.Size(52, 15);
             this.label4.TabIndex = 3;
             this.label4.Text = "Giá tiền:";
             // 
             // txtmaphong
             // 
             this.txtmaphong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtmaphong.Location = new System.Drawing.Point(163, 49);
-            this.txtmaphong.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtmaphong.Location = new System.Drawing.Point(122, 40);
             this.txtmaphong.Name = "txtmaphong";
-            this.txtmaphong.Size = new System.Drawing.Size(241, 24);
+            this.txtmaphong.Size = new System.Drawing.Size(182, 21);
             this.txtmaphong.TabIndex = 4;
             this.txtmaphong.TextChanged += new System.EventHandler(this.txtmaphong_TextChanged);
             // 
             // txttenphong
             // 
             this.txttenphong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txttenphong.Location = new System.Drawing.Point(163, 204);
-            this.txttenphong.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txttenphong.Location = new System.Drawing.Point(122, 166);
             this.txttenphong.Name = "txttenphong";
-            this.txttenphong.Size = new System.Drawing.Size(241, 24);
+            this.txttenphong.Size = new System.Drawing.Size(182, 21);
             this.txttenphong.TabIndex = 5;
             // 
             // txtdientich
             // 
             this.txtdientich.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtdientich.Location = new System.Drawing.Point(163, 125);
-            this.txtdientich.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtdientich.Location = new System.Drawing.Point(122, 102);
             this.txtdientich.Name = "txtdientich";
-            this.txtdientich.Size = new System.Drawing.Size(241, 24);
+            this.txtdientich.Size = new System.Drawing.Size(182, 21);
             this.txtdientich.TabIndex = 6;
             // 
             // txtgiatien
             // 
             this.txtgiatien.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtgiatien.Location = new System.Drawing.Point(163, 279);
-            this.txtgiatien.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtgiatien.Location = new System.Drawing.Point(122, 227);
             this.txtgiatien.Name = "txtgiatien";
-            this.txtgiatien.Size = new System.Drawing.Size(241, 24);
+            this.txtgiatien.Size = new System.Drawing.Size(182, 21);
             this.txtgiatien.TabIndex = 7;
             // 
             // dgvUsers
             // 
             this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvUsers.Location = new System.Drawing.Point(429, 38);
-            this.dgvUsers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvUsers.Location = new System.Drawing.Point(322, 31);
             this.dgvUsers.Name = "dgvUsers";
             this.dgvUsers.RowHeadersWidth = 51;
-            this.dgvUsers.Size = new System.Drawing.Size(537, 425);
+            this.dgvUsers.Size = new System.Drawing.Size(403, 345);
             this.dgvUsers.TabIndex = 8;
             this.dgvUsers.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsers_CellEnter);
             this.dgvUsers.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsers_RowEnter);
             // 
             // btnAddnew
             // 
-            this.btnAddnew.Location = new System.Drawing.Point(33, 360);
-            this.btnAddnew.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAddnew.Location = new System.Drawing.Point(25, 292);
             this.btnAddnew.Name = "btnAddnew";
-            this.btnAddnew.Size = new System.Drawing.Size(100, 28);
+            this.btnAddnew.Size = new System.Drawing.Size(75, 23);
             this.btnAddnew.TabIndex = 9;
             this.btnAddnew.Text = "Addnew";
             this.btnAddnew.UseVisualStyleBackColor = true;
             this.btnAddnew.Click += new System.EventHandler(this.btnAddnew_Click);
             // 
-            // btnExit
-            // 
-            this.btnExit.Location = new System.Drawing.Point(304, 421);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(100, 28);
-            this.btnExit.TabIndex = 10;
-            this.btnExit.Text = "Exit";
-            this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
-            // 
             // btncancel
             // 
-            this.btncancel.Location = new System.Drawing.Point(33, 421);
-            this.btncancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btncancel.Location = new System.Drawing.Point(25, 342);
             this.btncancel.Name = "btncancel";
-            this.btncancel.Size = new System.Drawing.Size(100, 28);
+            this.btncancel.Size = new System.Drawing.Size(75, 23);
             this.btncancel.TabIndex = 11;
             this.btncancel.Text = "Cancel";
             this.btncancel.UseVisualStyleBackColor = true;
@@ -174,10 +152,9 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(304, 360);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Location = new System.Drawing.Point(228, 292);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(100, 28);
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 12;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -185,10 +162,9 @@
             // 
             // btnEdit
             // 
-            this.btnEdit.Location = new System.Drawing.Point(173, 360);
-            this.btnEdit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEdit.Location = new System.Drawing.Point(130, 292);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(100, 28);
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
             this.btnEdit.TabIndex = 13;
             this.btnEdit.Text = "Edit";
             this.btnEdit.UseVisualStyleBackColor = true;
@@ -196,25 +172,34 @@
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(173, 421);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDelete.Location = new System.Drawing.Point(130, 342);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(100, 28);
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
             this.btnDelete.TabIndex = 14;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(228, 342);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 10;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnExit_Click);
+            // 
             // phongtro
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1003, 501);
+            this.ClientSize = new System.Drawing.Size(752, 407);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btncancel);
-            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnFind);
             this.Controls.Add(this.btnAddnew);
             this.Controls.Add(this.dgvUsers);
             this.Controls.Add(this.txtgiatien);
@@ -226,7 +211,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "phongtro";
             this.Text = "phongtro";
             this.Load += new System.EventHandler(this.phongtro_Load);
@@ -248,10 +232,10 @@
         private System.Windows.Forms.TextBox txtgiatien;
         private System.Windows.Forms.DataGridView dgvUsers;
         private System.Windows.Forms.Button btnAddnew;
-        private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btncancel;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFind;
     }
 }
--- WindowsFormsApp3/phongtro.cs
+++ WindowsFormsApp3/phongtro.cs
@@ -4,15 +4,17 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp3
 {
     public partial class phongtro : Form
     {
-        Database db = null;
+        internal Database db = null;
         public phongtro(Database dbinput)
         {
             InitializeComponent();
@@ -32,20 +34,23 @@ namespace WindowsFormsApp3
         }
         private void LoadGridData ()
         {
-            Database db = new Database();
-            string sql = "SELECT * from phongtro order by dientich";
+            if(string.IsNullOrEmpty(txtmaphong.Text))
+            string sql = "SELECT * from phongtro where ";
             dgvUsers.DataSource = db.getData(sql);
             setEnable(false);
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-            DialogResult dr;
+            /*DialogResult dr;
             dr = MessageBox.Show("Bạn muốn thoát chương trình!", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
             if (dr == DialogResult.Yes)
             {
                 this.Close();
-            }
+            }*/
+            string ssql = $" Select * from phongtro where ";
+            db.runQuery(ssql);
+            LoadGridData();
         }
 
         private void dgvUsers_RowEnter(object sender, DataGridViewCellEventArgs e)
@@ -57,16 +62,17 @@ namespace WindowsFormsApp3
         {
             int i = e.RowIndex;
             txtdientich.Text = dgvUsers.Rows[i].Cells["dientich"].Value.ToString();
-            txtgiatien.Text = dgvUsers.Rows[i].Cells["giatien"].Value.ToString();
+            txtgiatien.Text = dgvUsers.Rows[i].Cells["giatienphong"].Value.ToString();
             txtmaphong.Text = dgvUsers.Rows[i].Cells["maphong"].Value.ToString();
             txttenphong.Text = dgvUsers.Rows[i].Cells["tenphong"].Value.ToString();
         }
 
         private void btnAddnew_Click(object sender, EventArgs e)
         {
-           
-          
-                btnAddnew.Enabled = false;
+                btnDelete.Enabled = false;
+                btnEdit.Enabled = false;
+            //btnAddnew.Enabled = false;
+                btnEdit.Enabled = false;
                 setEnable(true);
                 txtdientich.Clear();
                 txtgiatien.Clear();
@@ -77,58 +83,82 @@ namespace WindowsFormsApp3
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            if (txttenphong.Text.Trim() == "")
+            try
             {
-                MessageBox.Show("Thông tin tên phòng không được để trống!","Thông báo",MessageBoxButtons.OK,MessageBoxIcon.Error);
-                txttenphong.Focus();
-                return;
+                if (txttenphong.Text.Trim() == "")
+                {
+                    MessageBox.Show("Thông tin tên phòng không được để trống!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txttenphong.Focus();
+                    return;
+                }
+                string uid = txttenphong.Text;
+                string fn = txtmaphong.Text;
+                string un = txtgiatien.Text;
+                string pw = txtdientich.Text;
+                if (btnAddnew.Enabled == true)
+                {
+                    string ssql = string.Format("SET IDENTITY_INSERT phongtro ON;insert into phongtro(maphong,tenphong,giatienphong,dientich)values" +
+                        "(N'{0}',N'{1}',N'{2}',N'{3}'); SET IDENTITY_INSERT phongtro OFF;", fn, uid, un, pw);
+                    Database db = new Database();
+                    db.runQuery(ssql);
+                    LoadGridData();
+                }
+                if (btnEdit.Enabled == true)
+                {
+                    int r = dgvUsers.CurrentRow.Index;
+                    string id = dgvUsers.Rows[r].Cells[0].Value.ToString();
+                    string ssql = string.Format("Update phongtro SET" +
+                                            " tenphong = N'{0}'," +
+                                            " maphong = N'{1}'," +
+                                            " giatienphong = N'{2}'," +
+                                            " dientich = N'{3}'"
+                                            + $"where maphong = {fn}"
+                                            , uid, fn, un, pw);
+                    Database db = new Database();
+                    db.runQuery(ssql);
+                    LoadGridData();
+                }
+                btnAddnew.Enabled = true;
+                btnSave.Enabled = true;
+                btnEdit.Enabled = true;
+                btnDelete.Enabled = true;
             }
-            string uid = txttenphong.Text;
-            string fn = txtmaphong.Text;
-            string un = txtgiatien.Text;
-            string pw = txtdientich.Text;
-            if(btnAddnew.Enabled)
-            {
-                string ssql = string.Format("insert into phongtro(maphong,tenphong,giatien,dientich)values" +
-                    "(N'{0}',N'{1}',N'{2}',N'{3}')", uid, fn, un, pw);
-                Database db = new Database();
-                db.runQuery(ssql);
-                LoadGridData();
-            }
-            else
-            {
-                int r = dgvUsers.CurrentRow.Index;
-                string id = dgvUsers.Rows[r].Cells[0].Value.ToString();
-                string ssql = string.Format("Update phongtro SET" +
-                                        " tenphong = N'{0}'," +
-                                        " maphong = N'{1}'," +
-                                        " giatien = N'{2}'," +
-                                        " dientich = N'{3}'", uid, fn, un, pw);
-                Database db = new Database();
-                db.runQuery(ssql);
-                LoadGridData();                         
-            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message.ToString());
+            };
         }
         private void btnDelete_Click(object sender, EventArgs e)
         {
             int r = dgvUsers.CurrentRow.Index;
-            string uid = dgvUsers.Rows[r].Cells[0].Value.ToString();
-            string sSql = string.Format("DELETE FROM phongtro where tenphong={0}",uid);
-            Database db = new Database();
-            db.runQuery(sSql);
-            LoadGridData();
-
+            try
+            { 
+                string uid = dgvUsers.Rows[r].Cells[0].Value.ToString();
+                string sSql = $"DELETE FROM phongtro where maphong={uid}";
+                Database db = new Database();
+                db.runQuery(sSql);
+                LoadGridData();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($" Lỗi dòng {r} đã nằm trong bảng thanh toán, bạn có thể muốn xóa dữ liệu trong bảng thanh toán trước ");
+            }
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
             btnAddnew.Enabled = false;
+            btnDelete.Enabled = false;
             setEnable(true);
+
         }
 
         private void btncancel_Click(object sender, EventArgs e)
         {
-            setEnable(false);
+            setEnable(true);
+            btnAddnew.Enabled = true;
+            btnSave.Enabled = true;
+            btnEdit.Enabled = true;
+            btnDelete.Enabled = true;
         }
 
         private void txtmaphong_TextChanged(object sender, EventArgs e)
