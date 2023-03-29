971a44abc0ed59d61bbb204eb18955f7c4049a2f
Kourain <maiquyen16503@gmail.com>
2023-03-29 09:18:41 +0700

    chang name

--- WindowsFormsApp3/Main.Designer.cs
+++ WindowsFormsApp3/Main.Designer.cs
@@ -176,7 +176,7 @@
             this.Controls.Add(this.ControlTab);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Main";
-            this.Text = "Form1";
+            this.Text = "Main";
             this.ControlTab.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
