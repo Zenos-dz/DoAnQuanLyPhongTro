ca621c1c0d78e40e2cba545bc8e8efd01801e3b0
Kourain <maiquyen16503@gmail.com>
2023-03-30 15:00:58 +0700

    change

--- DoAncs/phongtro.Designer.cs
+++ DoAncs/phongtro.Designer.cs
@@ -50,10 +50,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(51, 52);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(38, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 18);
+            this.label1.Size = new System.Drawing.Size(69, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mã phòng: ";
             // 
@@ -61,10 +60,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label2.Location = new System.Drawing.Point(51, 207);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(38, 168);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 18);
+            this.label2.Size = new System.Drawing.Size(69, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "Tên phòng:";
             // 
@@ -72,10 +70,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label3.Location = new System.Drawing.Point(60, 128);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(45, 104);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 18);
+            this.label3.Size = new System.Drawing.Size(58, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "Diện tích:";
             // 
@@ -83,68 +80,61 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label4.Location = new System.Drawing.Point(67, 282);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(50, 229);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 18);
+            this.label4.Size = new System.Drawing.Size(52, 15);
             this.label4.TabIndex = 3;
             this.label4.Text = "Giá tiền:";
             // 
             // txtmaphong
             // 
             this.txtmaphong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtmaphong.Location = new System.Drawing.Point(163, 49);
-            this.txtmaphong.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtmaphong.Location = new System.Drawing.Point(122, 40);
             this.txtmaphong.Name = "txtmaphong";
-            this.txtmaphong.Size = new System.Drawing.Size(241, 24);
+            this.txtmaphong.Size = new System.Drawing.Size(182, 21);
             this.txtmaphong.TabIndex = 4;
             this.txtmaphong.TextChanged += new System.EventHandler(this.txtmaphong_TextChanged);
             // 
             // txttenphong
             // 
             this.txttenphong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txttenphong.Location = new System.Drawing.Point(163, 204);
-            this.txttenphong.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txttenphong.Location = new System.Drawing.Point(122, 166);
             this.txttenphong.Name = "txttenphong";
-            this.txttenphong.Size = new System.Drawing.Size(241, 24);
+            this.txttenphong.Size = new System.Drawing.Size(182, 21);
             this.txttenphong.TabIndex = 5;
             // 
             // txtdientich
             // 
             this.txtdientich.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtdientich.Location = new System.Drawing.Point(163, 126);
-            this.txtdientich.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtdientich.Location = new System.Drawing.Point(122, 102);
             this.txtdientich.Name = "txtdientich";
-            this.txtdientich.Size = new System.Drawing.Size(241, 24);
+            this.txtdientich.Size = new System.Drawing.Size(182, 21);
             this.txtdientich.TabIndex = 6;
             // 
             // txtgiatien
             // 
             this.txtgiatien.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.txtgiatien.Location = new System.Drawing.Point(163, 279);
-            this.txtgiatien.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtgiatien.Location = new System.Drawing.Point(122, 227);
             this.txtgiatien.Name = "txtgiatien";
-            this.txtgiatien.Size = new System.Drawing.Size(241, 24);
+            this.txtgiatien.Size = new System.Drawing.Size(182, 21);
             this.txtgiatien.TabIndex = 7;
             // 
             // dgvUsers
             // 
             this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvUsers.Location = new System.Drawing.Point(429, 38);
-            this.dgvUsers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvUsers.Location = new System.Drawing.Point(322, 31);
             this.dgvUsers.Name = "dgvUsers";
             this.dgvUsers.RowHeadersWidth = 51;
-            this.dgvUsers.Size = new System.Drawing.Size(537, 425);
+            this.dgvUsers.Size = new System.Drawing.Size(403, 345);
             this.dgvUsers.TabIndex = 8;
             this.dgvUsers.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsers_CellEnter);
             this.dgvUsers.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsers_RowEnter);
             // 
             // btnAddnew
             // 
-            this.btnAddnew.Location = new System.Drawing.Point(33, 359);
-            this.btnAddnew.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAddnew.Location = new System.Drawing.Point(25, 292);
             this.btnAddnew.Name = "btnAddnew";
-            this.btnAddnew.Size = new System.Drawing.Size(100, 28);
+            this.btnAddnew.Size = new System.Drawing.Size(75, 23);
             this.btnAddnew.TabIndex = 9;
             this.btnAddnew.Text = "Addnew";
             this.btnAddnew.UseVisualStyleBackColor = true;
@@ -152,10 +142,9 @@
             // 
             // btncancel
             // 
-            this.btncancel.Location = new System.Drawing.Point(33, 421);
-            this.btncancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btncancel.Location = new System.Drawing.Point(25, 342);
             this.btncancel.Name = "btncancel";
-            this.btncancel.Size = new System.Drawing.Size(100, 28);
+            this.btncancel.Size = new System.Drawing.Size(75, 23);
             this.btncancel.TabIndex = 11;
             this.btncancel.Text = "Cancel";
             this.btncancel.UseVisualStyleBackColor = true;
@@ -163,10 +152,9 @@
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(304, 359);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Location = new System.Drawing.Point(228, 292);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(100, 28);
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 12;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -174,10 +162,9 @@
             // 
             // btnEdit
             // 
-            this.btnEdit.Location = new System.Drawing.Point(173, 359);
-            this.btnEdit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEdit.Location = new System.Drawing.Point(130, 292);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(100, 28);
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
             this.btnEdit.TabIndex = 13;
             this.btnEdit.Text = "Edit";
             this.btnEdit.UseVisualStyleBackColor = true;
@@ -185,10 +172,9 @@
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(173, 421);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDelete.Location = new System.Drawing.Point(130, 342);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(100, 28);
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
             this.btnDelete.TabIndex = 14;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
@@ -196,10 +182,9 @@
             // 
             // btnFind
             // 
-            this.btnFind.Location = new System.Drawing.Point(304, 421);
-            this.btnFind.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnFind.Location = new System.Drawing.Point(228, 342);
             this.btnFind.Name = "btnFind";
-            this.btnFind.Size = new System.Drawing.Size(100, 28);
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
             this.btnFind.TabIndex = 10;
             this.btnFind.Text = "Find";
             this.btnFind.UseVisualStyleBackColor = true;
@@ -207,9 +192,9 @@
             // 
             // phongtro
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1003, 501);
+            this.ClientSize = new System.Drawing.Size(752, 407);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.btnSave);
@@ -226,7 +211,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "phongtro";
             this.Text = "phongtro";
             this.Load += new System.EventHandler(this.phongtro_Load);
