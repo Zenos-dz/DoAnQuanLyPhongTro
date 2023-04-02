61c3a83a5c77ec369de5ab16640d562bd441ccc9
hiu <nguyenvanhieu13102003@gmail.com>
2023-04-02 21:43:16 +0700

    hieuuuu

--- DoAncs/Main.Designer.cs
+++ DoAncs/Main.Designer.cs
@@ -185,11 +185,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1261, 673);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.splitter1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Main";
---
+++ DoAncs/dangnhap.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Doancs
+{
+    partial class dangnhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dangnhap));
+            this.user = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // user
+            // 
+            this.user.BackColor = System.Drawing.Color.Red;
+            this.user.Location = new System.Drawing.Point(283, 305);
+            this.user.Multiline = true;
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(297, 34);
+            this.user.TabIndex = 0;
+            this.user.TextChanged += new System.EventHandler(this.user_TextChanged);
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(283, 363);
+            this.pass.Multiline = true;
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(297, 38);
+            this.pass.TabIndex = 1;
+            this.pass.TextChanged += new System.EventHandler(this.pass_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(327, 423);
+            this.button1.Margin = new System.Windows.Forms.Padding(40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Đăng Nhập";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dangnhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(835, 575);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "dangnhap";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.Button button1;
+    }
+}
